7d7ed2ce02f5afee988621f0e10b066697d6083b
Shirshendu Chakrabarti <shirshendu.c@directi.com>
2014-03-17 20:18:31 +0530

    modified the script to make request rensponse pattern evident and non blocking

--- test_inproc_reqrep.py
+++ test_inproc_reqrep.py
@@ -3,6 +3,7 @@
 import os
 import sys
 import time
+import random
 import pdb
 
 import nanomsg as nn
@@ -19,15 +20,33 @@ s1.bind("inproc://test-1")
 s2.connect("inproc://test-1")
 s3.connect("inproc://test-1")
 
+client_list = ["s2", "s3"]
 while(1):
 
-  # start communicating
-  s2.send("hello from socket, s2")
-  s3.send("hello from socket, s3")
+  # choose the client to communicate
+  client = random.choice(client_list)
 
-  # field the request
-  req = s1.recv()
-  print req  
+  if client == 's2':
+
+    # start communicating
+    s2.send("hello from socket, s2")
+    # field the request
+    print s1.recv()
+    # send a reponse back
+    resp = 'hello from socket, s1'
+    s1.send(resp)
+    print s2.recv()
+
+  elif client == 's3':
+
+    # start communicating
+    s3.send("hello from socket, s3")
+    # field the request
+    print s1.recv()
+    # send a reponse back
+    resp = 'hello from socket, s1'
+    s1.send(resp)
+    print s3.recv()
 
   time.sleep(1)  
 
