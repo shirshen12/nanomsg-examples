89a3f121793a6e899a71bb78a8a3f14fa597f9e1
Shirshendu Chakrabarti <shirshendu.c@directi.com>
2014-03-19 11:40:09 +0530

    + stats for msg processing

--- ipc/reqrep/test_ipc_reqrep_client.py
+++ ipc/reqrep/test_ipc_reqrep_client.py
@@ -2,6 +2,7 @@
 
 import os
 import sys
+import time
 import nanomsg as nn
 
 def main():
@@ -15,15 +16,30 @@ def main():
   # status message for declaring server comming up online
   print "An REQ client is connecting to REP server at ipc://test-ipc-sock"
 
-  while (1):
+  # global counters for messaging stats
+  client_sock_msg_processed = 0
 
-    # send a message to the server
-    msg = 'Hi! from a client with os level PID: %s' % os.getpid()
-    msg = client_sock.send(msg)
+  try:
 
-    # process the response from the server
-    print client_sock.recv()
+    start_time = time.time()
 
+    while (1):
+
+      # send a message to the server
+      msg = 'Hi! from a client with os level PID: %s' % os.getpid()
+      msg = client_sock.send(msg)
+
+      # process the response from the server
+      print client_sock.recv()
+
+      client_sock_msg_processed = client_sock_msg_processed + 1
+
+  except KeyboardInterrupt, e:
+    end_time = time.time()
+    delta = end_time - start_time
+
+    print 'total messages processed by client_sock: %s' % client_sock_msg_processed
+    print 'messages processed by client_sock per second: %s' % str(int(client_sock_msg_processed / delta))
 
 if __name__ == '__main__':
   main()
