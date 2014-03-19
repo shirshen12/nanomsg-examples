bd0d7c9cbffdcdd4686dec96746be2e33487150f
Shirshendu Chakrabarti <shirshendu.c@directi.com>
2014-03-19 11:10:33 +0530

    + stats logic to obtain messages processed in a total time and unit time

--- inproc/pair/test_inproc_pair.py
+++ inproc/pair/test_inproc_pair.py
@@ -17,15 +17,33 @@ s1.bind("inproc://test-1")
 # connect the socket
 s2.connect("inproc://test-1")
 
-# start communicating
-while (1):
+# counters for message processing
+s1_msg_processed = 0
+s2_msg_processed = 0
 
-  s1.send("hello from socket, s1")
-  s2.send("hello from socket, s2")
+try:
 
-  print s1.recv()
-  print s2.recv()
+  start_time = time.time()
 
-  time.sleep(1)
+  # start communicating
+  while (1):
 
+    s1.send("hello from socket, s1")
+    s2.send("hello from socket, s2")
+
+    print s1.recv()
+    print s2.recv()
+
+    s1_msg_processed = s1_msg_processed + 1
+    s2_msg_processed = s2_msg_processed + 1
+
+except KeyboardInterrupt, e:
+
+  end_time = time.time()
+  
+  print 'total message processed by s1: %s' % str(s1_msg_processed) 
+  print 'total message processed by s2: %s' % str(s2_msg_processed) 
+
+  print 'message processed by s1 per second: %s' % str(s1_msg_processed / (end_time - start_time))
+  print 'message processed by s2 per second: %s' % str(s2_msg_processed / (end_time - start_time))
 
